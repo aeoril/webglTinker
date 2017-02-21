976c90d78116f7e253f94da52aaa110a59bd3a2f
Aeoril <aeoril@ic3dimensions.com>
2017-02-21 13:09:03 -0600

    Updated copyriight notices in source code

--- webglBeginnings/010-rotation/glsl/webglBeginnings010_fs.glsl
+++ webglBeginnings/010-rotation/glsl/webglBeginnings010_fs.glsl
@@ -1,3 +1,9 @@
+/**
+ * @author aeoril | https://www.ic3dimensions.com
+ * Copyright © 2017 by IC3 Dimensions.  MIT License. See LICENSE.md
+ * Portions Copyright © 2012 by Gregg Tavares.  See LICENSE-webfundamentals.md
+ */
+
 // need to set default precision
 precision mediump float;
 varying vec4 v_color;
--- webglBeginnings/010-rotation/glsl/webglBeginnings010_vs.glsl
+++ webglBeginnings/010-rotation/glsl/webglBeginnings010_vs.glsl
@@ -1,3 +1,9 @@
+/**
+ * @author aeoril | https://www.ic3dimensions.com
+ * Copyright © 2017 by IC3 Dimensions.  MIT License. See LICENSE.md
+ * Portions Copyright © 2012 by Gregg Tavares.  See LICENSE-webfundamentals.md
+ */
+
 attribute vec2 a_position;
 attribute vec4 a_color;
 
