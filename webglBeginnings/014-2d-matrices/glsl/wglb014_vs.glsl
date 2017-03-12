2c42fedf7eafbfc770b3a1f8441486854d494fa5
Aeoril <aeoril@ic3dimensions.com>
2017-03-11 19:11:15 -0600

    Updated shader for matrix math

--- webglBeginnings/014-2d-matrices/glsl/wglb014_vs.glsl
+++ webglBeginnings/014-2d-matrices/glsl/wglb014_vs.glsl
@@ -7,26 +7,17 @@
 attribute vec2 a_position;
 attribute vec4 a_color;
 
-uniform vec2 u_centerTranslation;
-uniform vec2 u_rotation;
-uniform vec2 u_translation;
 uniform vec2 u_resolution;
-uniform vec2 u_scale;
+uniform mat3 u_matrix;
 
 varying vec4 v_color;
 
 void main() {
 
-  vec2 transformedPosition = a_position * u_scale;
-
-  transformedPosition =  vec2(
-    transformedPosition.x * u_rotation.y + transformedPosition.y * u_rotation.x,
-    transformedPosition.y * u_rotation.y - transformedPosition.x * u_rotation.x);
-
-  transformedPosition = transformedPosition + u_centerTranslation + u_translation;
+  vec2 position = ( u_matrix * vec3( a_position, 1 ) ).xy;
 
   // convert the position from pixels to 0.0 to 1.0
-  vec2 zeroToOne = transformedPosition / u_resolution;
+  vec2 zeroToOne = position / u_resolution;
 
   // convert from 0->1 to 0->2
   vec2 zeroToTwo = zeroToOne * 2.0;
@@ -39,6 +30,5 @@ void main() {
   // output position data
   gl_Position = vec4(clipSpaceFlipped, 0.0, 1.0);
 
-  // go from -1->+1 (clipspace) to 0->+1 (colorspace)
   v_color = a_color;
 }
