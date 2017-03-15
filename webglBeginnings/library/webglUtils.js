5ff3f0606567a8ed6e46a0d97f8f90f449f250b7
Aeoril <aeoril@ic3dimensions.com>
2017-03-15 17:55:10 -0500

    Updated authors in webglUtils.js

--- webglBeginnings/library/webglUtils.js
+++ webglBeginnings/library/webglUtils.js
@@ -1,4 +1,5 @@
 /**
+ * @author greggman | http://webglfundamentals.org
  * @author aeoril | https://www.ic3dimensions.com
  * Copyright © 2017 by IC3 Dimensions.  MIT License. See LICENSE.md
  * Portions Copyright © 2012 by Gregg Tavares.  See LICENSE-webfundamentals.md
