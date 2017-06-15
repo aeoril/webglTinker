21874892a151afac78c89046ef16bda94dfabe5c
Aeoril <aeoril@ic3dimensions.com>
2017-06-14 22:35:22 -0500

    rAFAnimate testing bug fixes, feature improvements, style updates

---
+++ webglBeginnings/019-rAFAnimateTesting/index.js
@@ -0,0 +1,138 @@
+window.addEventListener('load', function ( ) {
+
+  'use strict';
+
+  var outputElem = document.getElementById( 'output' );
+  var showTimesElem = document.getElementById( 'showTimes' );
+  var showTicksElem = document.getElementById( 'showTicks' );
+  var runElem = document.getElementById( 'run' );
+  var msPerTickElem = document.getElementById( 'msPerTick')
+  var msElem = document.getElementById( 'ms' );
+  var framesElem = document.getElementById( 'frames' );
+  var submitElem = document.getElementById( 'submit' );
+
+
+  var ticks = 0;
+  var ticksTTL = 0;
+  var framesPerTickCounter = -1;
+  var framesPerTick = 0;
+  var ticksPerFrame = 0;
+  var msPerTickImmediate = 0;
+  var msPerTickAverage = 0;
+  var timeSinceLastTick = 0;
+
+  var animateRAFed = rAFAnimate(animate, {
+
+    showTimes: 0,
+    showTicks: 0
+
+  } );
+
+  function animate ( options ) {
+
+    var outputString;
+
+    ticksTTL += options.ticks;
+
+    if (options.ticks) {
+
+      framesPerTickCounter++;
+
+      ticks = options.ticks;
+      ticksPerFrame = ticks / framesPerTickCounter;
+      framesPerTick = framesPerTickCounter / options.ticks;
+      timeSinceLastTick += options.deltaTime;
+      msPerTickImmediate = timeSinceLastTick / options.ticks;
+      msPerTickAverage = ( options.timestamp - options.startTime ) / ticksTTL;
+      timeSinceLastTick = 0;
+      framesPerTickCounter = 0;
+
+    } else {
+
+      timeSinceLastTick += options.deltaTime;
+      framesPerTickCounter++;
+
+    }
+
+    if ( options.showTimes) {
+
+      outputString =
+        'timestamp ......................: ' + options.timestamp.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'startTime ......................: ' + options.startTime.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'deltaTime ......................: ' + options.deltaTime.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'fps ............................: ' + ( 1000 / options.deltaTime ).toFixed( 2 ).padStart( 9, '0' ) + '\n\n';
+
+    } else {
+
+      outputString = '';
+
+    }
+
+    if ( options.showTicks ) {
+
+      outputString +=
+        'ticks at Sample Time ...........: ' + ticks.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'ticksTTL .......................: ' + ticksTTL.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'ticksPerFrame (calculated) .....: ' + ticksPerFrame.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'framesPerTick (calculated) .....: ' + framesPerTick.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'average msPerTick (calculated) .: ' + msPerTickAverage.toFixed( 2 ).padStart( 9, '0' ) + '\n' +
+        'immediate msPerTick (calculated): ' + msPerTickImmediate.toFixed( 2 ).padStart( 9, '0' );
+    }
+
+    outputElem.innerText = outputString;
+
+  }
+
+  function toggle ( elem, key ) {
+
+    var options = { };
+
+    options[key] = 'toggle';
+
+    if ( animateRAFed( options )[key] ) {
+
+      elem.style.borderStyle = 'inset';
+
+    } else {
+
+      elem.style.borderStyle = '';
+
+    }
+
+    return options;
+
+  }
+
+  showTimesElem.addEventListener( 'click', function ( ) { toggle( this, 'showTimes' ); }, false );
+  showTicksElem.addEventListener( 'click', function ( ) { toggle( this, 'showTicks' ); }, false );
+  runElem.addEventListener( 'click', function ( ) {
+
+    if ( toggle( this, 'repeat' ).repeat ) {
+
+      ticksTTL = 0;
+      framesPerTickCounter = -1;
+      framesPerTick = 0;
+      msPerTickImmediate = 0;
+      msPerTickAverage = 0;
+      timeSinceLastTick = 0;
+    }
+  }, false );
+
+  function submitMsPerTick ( ) {
+
+    var ms = parseFloat( msElem.value );
+    var frames = parseFloat( framesElem.value );
+    var msPerTick = ms / frames;
+
+    animateRAFed( { msPerTick: msPerTick } );
+
+    return false;
+
+  }
+
+  msPerTickElem.addEventListener( 'submit', submitMsPerTick, false );
+  submitElem.addEventListener( 'submit', submitMsPerTick, false );
+
+  submitMsPerTick( );
+
+} );
